ab1602e953e7d98f822d5dc95df27528bcb60bb6
Arne Harren <ah@0xc0.de>
2026-01-18 13:31:04 +0100

    render the box with preview visuals

--- models/box/box.js
+++ models/box/box.js
@@ -3,12 +3,12 @@
 const { align } = require('@jscad/modeling').transforms;
 
 const { openBox } = require('../../lib/boxes');
-const { xRay } = require('../../lib/visuals');
+const { withPreviewVisuals } = require('../../lib/visuals');
 
 const main = (params) => {
     const box = align({}, openBox({ size: [50, 50, 20], wallThickness: 1, outerCornerRadius: 0.5, center: [0, 0, 0] }));
 
-    return box; // xRay(box);
+    return box;
 }
 
-module.exports = { main };
+module.exports = { ...withPreviewVisuals(main) };
